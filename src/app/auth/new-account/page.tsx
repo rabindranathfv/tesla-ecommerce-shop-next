93aad55d4f67177813fad7fec8d1b6e15ed67fd5
Rabindranath Ferreira <rabindranath.ferreira@ingka.com>
2026-07-25 18:07:53 +0200

    feat: base layout for login and newAccount

--- src/app/auth/new-account/page.tsx
+++ src/app/auth/new-account/page.tsx
@@ -1,25 +1,36 @@
 import { titleFont } from "@/config/fonts";
 import Link from "next/link";
 
-export default function LoginPage() {
+export default function newAccountPage() {
   return (
     <div className="flex flex-col min-h-screen pt-32 sm:pt-52">
-      <h1 className={`${titleFont.className} text-4xl mb-5`}>Sign In</h1>
+      <h1 className={`${titleFont.className} text-4xl mb-5`}>
+        Create a New Account
+      </h1>
 
       <div className="flex flex-col">
+        <label htmlFor="full-name">Full name</label>
+        <input
+          className="px-5 py-2 border bg-gray-200 rounded mb-5"
+          type="text"
+          id="full-name"
+        />
+
         <label htmlFor="email">Email</label>
         <input
           className="px-5 py-2 border bg-gray-200 rounded mb-5"
           type="email"
+          id="email"
         />
 
-        <label htmlFor="email">Password</label>
+        <label htmlFor="password">Password</label>
         <input
           className="px-5 py-2 border bg-gray-200 rounded mb-5"
-          type="email"
+          type="password"
+          id="password"
         />
 
-        <button className="btn-primary">Sign In</button>
+        <button className="btn-primary">Create Account</button>
 
         {/* divisor line */}
         <div className="flex items-center my-5">
@@ -28,8 +39,8 @@ export default function LoginPage() {
           <div className="flex-1 border-t border-gray-500"></div>
         </div>
 
-        <Link href="/auth/new-account" className="btn-secondary text-center">
-          Create a new account
+        <Link href="/auth/login" className="btn-secondary text-center">
+          Sign In
         </Link>
       </div>
     </div>
